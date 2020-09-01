b0dd39bc8128ad83a1f411e1845673b3ae9fbc56
Sharoon Thomas <sharoon.thomas@fulfil.io>
2020-09-01 06:36:04 -0400

    Bump version: 0.1.3 → 0.1.4

--- pymws/__init__.py
+++ pymws/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fulfil.IO Inc."""
 __email__ = 'help@fulfil.io'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .pymws import MWS              # noqa
 from .exceptions import MWSError    # noqa
