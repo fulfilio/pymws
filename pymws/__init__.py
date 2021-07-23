bf256f886ffd75a851cb059c025abe00e6bf5cc4
Anmol Gulati <anmol.gulati9513@gmail.com>
2021-07-23 17:33:02 +0530

    Bump version: 0.1.11 → 0.1.12

--- pymws/__init__.py
+++ pymws/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fulfil.IO Inc."""
 __email__ = 'help@fulfil.io'
-__version__ = '0.1.11'
+__version__ = '0.1.12'
 
 from .pymws import MWS              # noqa
 from .exceptions import MWSError    # noqa
