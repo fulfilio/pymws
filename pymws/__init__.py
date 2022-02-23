09179130d370387252a79952d11b21549f2e87cc
Anmol Gulati <anmol.gulati9513@gmail.com>
2022-02-23 19:20:05 +0530

    Bump version: 0.1.13 → 0.2.0

--- pymws/__init__.py
+++ pymws/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fulfil.IO Inc."""
 __email__ = 'help@fulfil.io'
-__version__ = '0.1.13'
+__version__ = '0.2.0'
 
 from .pymws import MWS              # noqa
 from .exceptions import MWSError    # noqa
--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     packages=find_packages(include=['pymws', 'pymws.*']),
     test_suite='tests',
     url='https://github.com/fulfilio/pymws',
-    version='0.1.13',
+    version='0.2.0',
     zip_safe=False,
 )
